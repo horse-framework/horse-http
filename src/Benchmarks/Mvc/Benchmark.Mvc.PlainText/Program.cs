dee59f065d0478e72fd044589d96b1f17828e4b8
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-07-12 23:44:04 +0300

    benchmark projects are moved

---
+++ src/Benchmarks/Mvc/Benchmark.Mvc.PlainText/Program.cs
@@ -0,0 +1,18 @@
+﻿using Twino.Mvc;
+using Twino.Server;
+
+namespace Benchmark.Mvc.PlainText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwinoMvc mvc = new TwinoMvc();
+            TwinoServer server = new TwinoServer();
+            mvc.Init();
+            server.UseMvc(mvc);
+            server.Start(5000);
+            server.BlockWhileRunning();
+        }
+    }
+}
